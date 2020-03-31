03ea269fafee98dbd811352f823998f7445b8124
eito-fis <eamon@itofisher.com>
2020-03-31 02:33:57 -0400

    Created prototype for training loop object

---
+++ training_loop.py
@@ -0,0 +1,14 @@
+class TrainingLoop:
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def build_model(self):
+        pass
+
+    def build_loaders(self):
+        pass
+
+
