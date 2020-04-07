15efdd526281fb10029fb17d2576612a6fab74d6
eito-fis <eamon@itofisher.com>
2020-04-07 12:17:57 -0400

    Added support for non-sequential data to be appended to the output of the LSTM module in the LSTM Net class

--- ml_lib/lstm_net.py
+++ ml_lib/lstm_net.py
@@ -24,6 +24,7 @@ class LSTM_Net(Model):
                  output_dim,
                  hidden_dim,
                  n_layers,
+                 non_seq_dim=None,
                  dropout=0.5,
                  out_actv=None):
         """
@@ -35,6 +36,8 @@ class LSTM_Net(Model):
             output_dim: Output dimensions
             hidden_dim: LSTM hidden dimensions
             n_layers: Number of LSTM layers
+            non_seq_dim: Non sequential data dimensions. Use if you want to
+            append extra data onto the output of the LSTM module
             dropout: Probability to use for dropout layers. Defaults to 0.5
             out_actv: Pytroch activation function to use on final output. Is not
             called if None
@@ -52,7 +55,7 @@ class LSTM_Net(Model):
         self.lstm = nn.LSTM(input_dim, hidden_dim, n_layers,
                             dropout=dropout, batch_first=True)
 
-        prev_out = hidden_dim
+        prev_out = hidden_dim + (non_seq_dim if non_seq_dim else 0)
         self.fc = []
         for i,neurons in enumerate(fc):
             self.fc.append(nn.Linear(prev_out, neurons))
@@ -62,15 +65,17 @@ class LSTM_Net(Model):
         # Convert parameters to float
         self.float()
 
-    def forward(self, x):
+    def forward(self, x, non_seq=None):
         """
         Forward pass of the network
 
         Parameters:
-            x: Input of size (batch, seq_length, input_size)
+            x: Input of size (batch, seq_length, input_dim)
+            non_seq: Optional input of non sequential data to be appended after
+            the LSTM module, of size (batch, non_seq_dim)
 
         Returns:
-            Tensor: Output of size (batch, output_size)
+            Tensor: Output of size (batch, output_dim)
         """
         x = x.float()
         batch_size = x.size(0)
@@ -78,7 +83,12 @@ class LSTM_Net(Model):
         # Save only the final hidden state for each sequence
         lstm_out = lstm_out[:, -1, :]
 
-        dense = lstm_out
+        # Append non_sequential data after LSTM processing
+        if non_seq is None:
+            dense = lstm_out
+        else:
+            non_seq = non_seq.float()
+            dense = torch.cat((lstm_out, non_seq), dim=1)
         for l in self.fc:
             dense = l(dense)
             dense = self.actv(dense)
@@ -111,13 +121,17 @@ if __name__ == "__main__":
     input_dim = 100
     batch_size = 32
     seq_len = 16
+    non_seq_dim = 5
     model = LSTM_Net(fc=[128],
                      input_dim=input_dim,
                      output_dim=1,
                      hidden_dim=128,
                      n_layers=1,
-                     drop_prob=0.5)
+                     non_seq_dim=non_seq_dim,
+                     dropout=0.5)
     import numpy as np
     inputs = np.random.random((batch_size, seq_len, input_dim))
+    non_seq = np.random.random((batch_size, non_seq_dim))
     inputs = torch.from_numpy(inputs)
-    out = model(inputs)
+    non_seq = torch.from_numpy(non_seq)
+    out = model(inputs, non_seq=non_seq)
