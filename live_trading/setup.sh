d1ddeadfb8534bc37673bd23fca5f3d59a0e39c8
intermezzio <amascillaro@gmail.com>
2020-04-21 15:54:05 -0400

    Fix setup.sh script for live trading

--- live_trading/setup.sh
+++ live_trading/setup.sh
@@ -1,12 +1,3 @@
 
-rm -rf stock-market-prediction
-git rm --cached stock-market-prediction
-rm .gitmodules
-touch .gitmodules
-git config -f .git/config --remove-section submodule.stock-market-prediction
-git submodule add https://github.com/intermezzio/stock-market-prediction
-cd ./stock-market-prediction
-sh setup.sh
-cd ..
 mkdir config
 printf "APCA_API_KEY_ID = 'your-api-key'\nAPCA_API_SECRET_KEY = 'your-secret-key'\nALPHAVANTAGE_API_KEY = 'your-alphavantage-token'\n\nAPCA_API_BASE_URL = 'https://paper-api.alpaca.markets'" >> config/config.py
