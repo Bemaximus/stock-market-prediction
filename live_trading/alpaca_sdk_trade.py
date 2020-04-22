952ec5d742c1e36474afbf6dde9bf3007ceb32bc
intermezzio <amascillaro@gmail.com>
2020-04-22 01:36:04 -0400

    Connect the algorithm to the paper trading account

--- live_trading/alpaca_sdk_trade.py
+++ live_trading/alpaca_sdk_trade.py
@@ -8,6 +8,8 @@ import time
 import pytz
 import requests
 import random
+import numpy as np
+import pandas as pd
 import alpaca_trade_api as tradeapi
 
 from config import *
@@ -34,24 +36,37 @@ def opening_buys(symbols=["JNUG", "JDST"], account_money=None):
 	Using the opening price and 2 weeks of historical data, choose what to buy
 
 	"""
+	# Amount of money to invest
 	if account_money == None:
 		account_money = float(api.get_account().cash)
+
+	# Store current stock data for sending to the algorithm
+	# and preparing transactions
 	est_increases = dict()
 	current_prices = dict()
+	historical_prices = dict()
+
+	# Iterate through each symbol and get
+	# a) current price
+	# b) last 10 days' quotes
+	# c) estimated percent increase
 	for symbol in symbols:
-		current_prices[symbol] = float(api.alpha_vantage.current_quote(symbol)["05. price"])
+		est_increases[symbol], current_prices[symbol], _ = jonas_bonus(symbol)
+		# current_prices[symbol] = float(api.alpha_vantage.current_quote(symbol)["05. price"])
 		print(f"{symbol}: ${current_prices[symbol]}")
-		est_increases[symbol] = random.uniform(0.95, 1.05) # est_perc_increase(symbol, current_prices[symbol])
+		# est_increases[symbol] = random.uniform(0.95, 1.05) # est_perc_increase(symbol, current_prices[symbol])
 
 	buy_ticker = max(est_increases, key=est_increases.get)
 	print(buy_ticker)
 	print(est_increases[buy_ticker])
 	if est_increases[buy_ticker] > 1:
 		# buy this stock
-		r = api.submit_order(buy_ticker, account_money // current_prices[buy_ticker], 
+		r = api.submit_order(buy_ticker, (account_money * .95) // current_prices[buy_ticker], 
 			"buy", "market", "gtc")
 		print(account_money // current_prices[buy_ticker])
-		bought_stock_mail(r.symbol, r.qty, price=current_prices[buy_ticker], trade=r)
+		bought_stock_mail(r.symbol, r.qty, price=current_prices[buy_ticker],
+			trade=f"Buy: {buy_ticker}\nEstimated Increases: {est_increases}\n" + 
+			f"Current Prices: {current_prices}\n{r}")
 		return r
 	return 0
 
@@ -61,6 +76,30 @@ def liquidate():
 	if close:
 		liquidate_stock_mail(close) # email notification if there are any trades
 
+def jonas_bonus(ticker):
+	"""
+	Take a ticker and return the current price and percentage increase
+	Make an API call to Alphavantage to receive this data
+	"""
+	params = {
+		"symbol": ticker,
+		"adjusted": False,
+		"outputsize": "compact",
+		"cadence": "daily",
+		"output_format": "pandas"
+	}
+	historical_dataset = api.alpha_vantage.historic_quotes(**params)
+
+	last_ten_days = historical_dataset.iloc[10:0:-1, 0:4].values.flatten()
+	opening_price = historical_dataset.iloc[0, 0]
+	current_price = historical_dataset.iloc[0, 3]
+	normalizing_factor = last_ten_days[0]
+
+	algo_inputs = np.append(last_ten_days, opening_price) / normalizing_factor
+	print(algo_inputs)
+
+	price_change = est_perc_increase(ticker, algo_inputs)
+	return price_change, current_price, historical_dataset
 
 if __name__ == "__main__" and len(sys.argv) > 1 and sys.argv[1] == "auto":
 	while True:
