46ab22b031eee5fd264604032c3abbcf70d6d79e
Andrew Mascillaro <amascillaro@gmail.com>
2020-04-24 18:07:45 -0400

    Merge shell scripts into one large 'setup.sh'

--- live_trading/setup.sh
+++
@@ -1,2 +0,0 @@
-mkdir config
-printf "APCA_API_KEY_ID = 'your-api-key'\nAPCA_API_SECRET_KEY = 'your-secret-key'\nALPHAVANTAGE_API_KEY = 'your-alphavantage-token'\n\nAPCA_API_BASE_URL = 'https://paper-api.alpaca.markets'" >> config/config.py
--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
 sh setupworktrees.sh
+cd live_trading
+mkdir config
+printf "APCA_API_KEY_ID = 'your-api-key'\nAPCA_API_SECRET_KEY = 'your-secret-key'\n\nAPCA_API_BASE_URL = 'https://paper-api.alpaca.markets'\nEMAIL_ADDRESS = 'your-email-address'\nEMAIL_PASSWORD = 'your-email-password" >> config/config.py
+cd ..
