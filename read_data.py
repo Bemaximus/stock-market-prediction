871d7eba2aca6b432672883a758c8186eb4e3ace
intermezzio <amascillaro@gmail.com>
2020-05-09 16:25:42 -0400

    Automate some parts of testing the algorithm

--- read_data.py
+++ read_data.py
@@ -20,24 +20,30 @@ def save_data(stock_data):
 	"""
 	filtered_stock_data = stock_data.loc[:, ["Open", "High", "Low", "Close"]]
 	np_stock_data = filtered_stock_data.to_numpy()
-	np_stock_data = np.flipud(np_stock_data)[0:1000,:]
+	
+	# Separate the testing and training data
 	print(np_stock_data)
 	num_rows = np.size(np_stock_data, 0)
 	num_rows_train = num_rows - 250
 	Y_unformat = np_stock_data[0:num_rows_train, :]
 	T_unformat = np_stock_data[num_rows_train:num_rows, :]
+
+	# Determine the "answers" -> actual percentage increases
 	Q = np.divide(T_unformat[10:num_rows,[3]], T_unformat[10:num_rows,[0]])
 	C = np.divide(Y_unformat[10:num_rows_train,[3]], Y_unformat[10:num_rows_train,[0]])
+
+	# Initialize the test data arrays
 	Y = np.empty((0,41))
 	T = np.empty((0,41))
+
+	# Add training data one row at a time
 	for row in range(np.size(Y_unformat, 0)-10):
 		first_open = Y_unformat[row,[0]]
 		last_open = Y_unformat[row+10,[0]]
 		# row 'row+10' isn't used in the line below, only indices 0-9
-		formatted_row = Y_unformat[row:row+10, :]
+		unformatted_row = Y_unformat[row:row+10, :]
 		formatted_row = formatted_row.flatten()
-		formatted_row = np.append(formatted_row, last_open)
-		formatted_row = np.divide(formatted_row,first_open)
+		formatted_row = normalize_dataset(unformatted_row, last_open)
 		Y = np.append(Y, [formatted_row], axis=0)
 	for row in range(np.size(T_unformat, 0)-10):
 		first_open = T_unformat[row,[0]]
@@ -58,6 +64,30 @@ def save_data(stock_data):
 	with open(dir_path + f"/data/{ticker}_test_train.p", 'wb') as fp:
 		pickle.dump(model_dict, fp)
 	return model_dict
+
+def normalize_dataset(lookback, opening_price):
+	"""
+	Take training data and convert it into a line for the linear regression matrix
+	"""
+	print(lookback)
+	
+	# high, low, close: divide by opening price of the day
+
+	lookback[:, 1:] /= lookback[:,0]
+	print(lookback)
+
+	# open: divide by the previous open
+
+	lookback[1:, 0] /= lookback[:-1, 0]
+	print(lookback)
+
+	# first open should be 1
+	lookback[0, 0] = 1
+
+	flat_lookback = lookback.flatten()
+	full_flat_lookback = np.append(flat_lookback, opening_price)
+	
+	return full_flat_lookback
 
 if __name__ == "__main__":
 
